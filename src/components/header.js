e40346311d3b3925782fa4fc0dcd1d554a44b7e0
xian <iamchristianrusso@gmail.com>
2019-04-21 22:30:16 -0400

    working on header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,81 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+const SiteLogo = () => {
+  return (
+    <h1>
+      <Link
+        to='/'
+        style={{
+          color: `#313335`,
+          textDecoration: `none`,
+        }}
+      >
+        Xian
+      </Link>
+    </h1>
+  )
+}
+
+const Li = styled.li`
+  list-style-type: none;
+`
+
+const LinkItem = ({ page }) => {
+  return(
+    <Li><Link to='/{page}' 
+      style={{ 
+        textDecoration: `none`, 
+        color: `#313335`,
+        marginRight: `10px`, 
+      }}
+    >
+      {page}
+    </Link></Li>
+  )
+}
+
+const Header = () => (
+  <header>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        display: `flex`,
+        flexDirection: `row`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <SiteLogo 
+        style={{
+          marginRight: `auto`,
+        }}
+      />
+      <ul
+        style={{
+          padding: 0,
+          display: `flex`,
+          flexDirection: `row`,
+          justifyContent: `space-between`,
+          alignItems: `baseline`,
+        }}
+        role='navigation'
+      >
+        <LinkItem page='About' />
+        <LinkItem page='Work' />
+        <LinkItem page='Contact' />
+      </ul> 
     </div>
   </header>
 )
+
+const StyledHeader = styled(Header)`
+  margin-bottom: 1.25rem;
+  display: flex;
+  flex-direction: row;
+`
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -39,4 +85,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+export default StyledHeader
